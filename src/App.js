ced4647bb4af6272c92eeb237efc1e9445817091
Akash981616 <Ak98574586@gmail.com>
2022-07-24 19:12:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,54 +1,11 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { CircularProgress } from "@material-ui/core";
-import { setApiData, findById } from "../src/app/actions/Actions";
-import { useSelector, useDispatch } from "react-redux";
-import { Button } from "@material-ui/core";
-import "./App.css";
-import PersonCard from "./component/PersonCard";
-
+import React from 'react'
+//import Debouncing from './component/CurrencyExchange'
+import './App.css'
 const App = () => {
-  const { apidata } = useSelector((state) => state.taskReducer);
-  const dispatch = useDispatch();
-
-  const [loading, setLoading] = useState(null);
-
-  const fetchData = async () => {
-    setLoading(true);
-    const infodata = await axios.get("https://reqres.in/api/users?page=1");
-    dispatch(setApiData(infodata.data.data));
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
-    <div className="App">
-      <PersonCard />
-      {loading ? (
-        <CircularProgress color="secondary" size={104} />
-      ) : (
-        <h1>Please Click on Any Button</h1>
-      )}
-
-      <div className="page-div">
-        {apidata?.map((place, i) => (
-          <div className="page-idx" key={i}>
-            <Button
-              variant="contained"
-              key={i}
-              onClick={() => dispatch(findById(i))}
-            >
-              {i + 1}
-            </Button>
-          </div>
-        ))}
-      </div>
+    <div>
     </div>
   );
-};
+}
 
-export default App;
+export default App
